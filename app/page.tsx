68047ba2d06585418760d603773045a2f5e80c88
naybyal <officiallynabiel@gmail.com>
2024-09-24 23:15:39 +0530

    Update for September 2024

--- app/page.tsx
+++ app/page.tsx
@@ -16,6 +16,22 @@ const words = [
   {
     text: "here.",
   },
+  {
+    text: "I'm a",
+  },
+  {
+    text: "Full-stack",
+  },
+  {
+    text: "Engineer",
+  },
+  {
+    text: "and a",
+  },
+  {
+    text: "System Programmer.",
+  },
+
 
   
 ];
@@ -40,7 +56,7 @@ export default function Home() {
         className="absolute inset-0 -z-10 animate-fade-in"
         quantity={100}
       />
-      <h1 className="z-10 text-2xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
+      <h1 className="z-10 text-2xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text font-regular">
         <TypewriterEffectSmooth words={words} />
       </h1>
 
