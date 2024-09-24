41824dea0d5023ffd0cc0032b18dae7fa0c16af3
naybyal <officiallynabiel@gmail.com>
2024-09-24 23:07:50 +0530

    Update for September 2024

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,24 @@
 import Link from "next/link";
 import React from "react";
 import Particles from "./components/particles";
-
+import { TypewriterEffectSmooth } from "../app/components/typewriter-effect";
 const navigation = [
   { name: "Blog", href: "https://www.nabielwrites.co" },
   { name: "Contact", href: "/contact" }
 ];
+const words = [
+  {
+    text: "Hi!",
+  },
+  {
+    text: "Nabiel",
+  },
+  {
+    text: "here.",
+  },
 
+  
+];
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
@@ -29,7 +41,7 @@ export default function Home() {
         quantity={100}
       />
       <h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-        Hi, Nabiel here! 👋 
+        <TypewriterEffectSmooth words={words} />
       </h1>
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
