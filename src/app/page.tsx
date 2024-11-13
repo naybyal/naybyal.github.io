823097e4b5c0fa0875feec8cac222c0bd2a8b27d
naybyal <officiallynabiel@gmail.com>
2024-11-13 11:29:28 +0530

    update: November 2024

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ export const metadata: Metadata = {
   title: 'Nabiel Ahammed | Fullstack Developer & System Programmer',
   description: "Explore Nabiel Ahammed’s portfolio showcasing innovative projects in SaaS, TypeScript, Rust, and Fullstack Development expertise.",
   keywords: ['Nabiel Ahammed', 'Fullstack Developer', 'System Programmer', 'TypeScript', 'Rust', 'SaaS Development', 'Portfolio'],
-  author: 'Nabiel Ahammed',
   openGraph: {
     title: 'Nabiel Ahammed | Fullstack Developer & System Programmer',
     description: "Dive into Nabiel Ahammed’s professional portfolio featuring innovative SaaS solutions, expertise in TypeScript, Rust, and system programming.",
